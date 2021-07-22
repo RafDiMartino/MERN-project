7fbe1fd85a428a23c18a45396b6df1281fcf4e48
R4FFF <69420740+R4FFF@users.noreply.github.com>
2021-07-22 16:22:35 +0100

    redux setup

--- server/index.js
+++ server/index.js
@@ -7,17 +7,19 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes)
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes)
+
 const CONNECTION_URL = "mongodb+srv://rafdm:rafdm123@cluster0.dzpc9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
     .catch((error) => console.log(error.message));
 
 mongoose.set("useFindAndModify", false);
